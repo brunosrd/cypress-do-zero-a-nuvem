6c102c192f939ac5c8cde9e8a5e464b1faaaf899
Brunosrd <brunosr099@outlook.com>
2025-02-28 10:03:39 -0300

    Adicionando testes de selecionar arquivos

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -121,6 +121,23 @@ describe('Central de Atendimento ao Cliente TAT', () => {
         expect(input[0].files[0].name).to.equal('example.json')
       })
   })
+ 
+  it('seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('#file-upload')
+      .selectFile('cypress/fixtures/example.json',  { action: 'drag-drop' })
+      .then(input => {
+        expect(input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
+  it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture("example.json").as('arquivoExemplo')
+    cy.get('#file-upload')
+    .selectFile('@arquivoExemplo') //usamos @ para chamar alias
+    .then(input => {
+      expect(input[0].files[0].name).to.equal('example.json')
+    })
+  })
 
 
-})
+});
